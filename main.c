ff8ac2ea69e9791188558f9cf0ce509269f5fe9a
wiznux <wiznux@gmail.com>
2024-02-29 22:50:24 +0100

    Add wait loop

--- main.c
+++ main.c
@@ -2,7 +2,13 @@
 
 int main()
 {
-	printf("Hello world !\n);
+	int i;
+	for(i=0; i<100; i++)
+	{
+			printf("%i ", i);
+	}
+	
+	printf("\nHello world !\n);
 	printf("How are you ?\n");
 	return 0;
 }
